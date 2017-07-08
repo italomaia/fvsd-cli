ead91c792d4f09690b04a87ce8b89121e1f054a5
Italo Maia <italo.maia@gmail.com>
2017-07-08 14:51:06 -0300

    Updating package info

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@
 
 from distutils.core import setup
 
-description = """Flask+VueJS+SemanticUI+Docker CLI helper.
-Also, see https://github.com/italomaia/flask-vue-semantic-docker."""
+long_description = """
+Using the repository (https://github.com/italomaia/flask-vue-semantic-docker),
+this CLI is capable of easely creating a boilerplate project using
+Flask, VueJS, Nginx, SemanticUI and Docker in a easy manner.
+
+Just install this package and call fvsd.py new <project-name> to get started.
+""".strip()
 
 setup(
     name='fvsd',
     version='1.0',
-    description=description,
+    description="Flask+VueJS+SemanticUI+Docker CLI boilerplate.",
+    long_description=long_description,
     author='Italo Maia',
     author_email='italo.maia@gmail.com',
     url='https://github.com/italomaia/fvsd-cli',
