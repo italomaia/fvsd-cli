975088d7069bc4d4786efa5ac293c30c30832175
Italo Maia <italo.maia@gmail.com>
2017-07-08 14:51:39 -0300

    version bump 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Just install this package and call fvsd.py new <project-name> to get started.
 
 setup(
     name='fvsd',
-    version='1.0',
+    version='1.0.1',
     description="Flask+VueJS+SemanticUI+Docker CLI boilerplate.",
     long_description=long_description,
     author='Italo Maia',
