93438de1338d642cc6270823e3439d75c7f542bc
Italo Maia <italo.maia@gmail.com>
2017-12-26 19:28:27 -0300

    conditional requirement on python version

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 from distutils.core import setup
 
 long_description = """
@@ -10,6 +11,14 @@ Flask, VueJS, Nginx, SemanticUI and Docker in a easy manner.
 Just install this package and call fvsd.py new <project-name> to get started.
 """.strip()
 
+required = []
+
+if sys.version_info.major == 3:
+    required.append('fabric3')
+else:
+    required.append('fabric')
+
+
 setup(
     name='fvsd',
     version='1.0.3',
@@ -19,4 +28,5 @@ setup(
     author_email='italo.maia@gmail.com',
     url='https://github.com/italomaia/fvsd-cli',
     scripts=['fvsd.py'],
+    install_requires=required,
 )
